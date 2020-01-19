710c12c4d2efe1caad19b52516127402461bc3cd
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-19 22:50:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+let answer = array.find( (obj)=>
+  obj.result === "W")
+
+   if (answer){
+   return answer.year}
+   else {
+     return  undefined
+   }
+  }
